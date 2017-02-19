5479d0095335796b9756dafc67821a1e740f357b
Zhi Qiang <kyroguex@gmail.com>
2017-02-19 23:55:08 +0800

    Complete scss

--- app/components/Todo.jsx
+++ app/components/Todo.jsx
@@ -2,29 +2,42 @@ var React = require('react');
 var moment = require('moment');
 
 var Todo = React.createClass({
-    render:function(){
+    render: function () {
         var {text} = this.props;
         var {id} = this.props;
         var {completed} = this.props;
         var {createdAt} = this.props;
         var {completedAt} = this.props;
-
-        var renderDate=()=>{
+        var todoClassName = completed
+            ? 'todo todo-completed'
+            : 'todo';
+        var renderDate = () => {
             var message = 'Created ';
             var timestamp = createdAt;
-            if(completed){
-                message ='Completed';
+            if (completed) {
+                message = 'Completed ';
                 timestamp = completedAt;
             }
-            return message + moment.unix(timestamp).format('MMM Do YYYY @ h:mm a');
+            return message + moment
+                .unix(timestamp)
+                .format('MMM Do YYYY @ h:mm a');
         };
-        return(
-            <div onClick={()=>{
-                this.props.onToggle(id);
-                }}>
-                <input type="checkbox" checked={completed} />
-                <p>{text}</p>
-                <p>{renderDate()}</p>
+        return (
+            <div
+                className={todoClassName}
+                onClick={() => {
+                this
+                    .props
+                    .onToggle(id);
+            }}>
+                <div>
+                    <input type="checkbox" checked={completed}/>
+                </div>
+                <div>
+                    <p>{text}</p>
+                    <p className="todo__subtext">{renderDate()}</p>
+                </div>
+
             </div>
         );
     }
